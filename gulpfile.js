df1ea3cf82fac1d55fd90bd7a4aadd06f2ea4212
nasdan <daniel.sanchez@lemoncode.net>
2017-06-21 12:19:47 +0200

    Add basic gulp configuration with typescript

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const sourcemaps = require('gulp-sourcemaps');
+const nodemon = require('gulp-nodemon');
+const rimraf = require('rimraf');
+const runSequence = require('run-sequence');
+const tsConfig = ts.createProject('./tsconfig.json');
+
+const paths = {
+  ts: './src/**/*.ts',
+  dest: './public',
+  destIndex: './public/app.js',
+};
+
+gulp.task('clean', (callback) => (
+  rimraf(paths.dest, callback)
+));
+
+gulp.task('ts', () => (
+  gulp.src(paths.ts)
+    .pipe(sourcemaps.init())
+    .pipe(tsConfig())
+    .js
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.dest))
+));
+
+gulp.task('start', () => (
+  nodemon({
+    script: paths.destIndex,
+  })
+));
+
+gulp.task('watch', () => (
+  gulp.watch([paths.ts], ['ts'])
+));
+
+gulp.task('build', [
+  'clean',
+  'ts'
+]);
+
+gulp.task('dev', [
+  'watch',
+  'start'
+]);
---
+++ src/env.config.ts
@@ -0,0 +1,4 @@
+export const env = {
+  LM_NODE_ENV: process.env.NODE_ENV || "development",
+  LM_PORT: process.env.LM_PORT || 8081,
+};
