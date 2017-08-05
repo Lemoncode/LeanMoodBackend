94504807d41ccfdf30d7b7c2cbf2370f7820f408
nasdan <daniel.sanchez@lemoncode.net>
2017-08-05 12:51:33 +0200

    Return user when login success and add specs

--- src/routers/login/router.ts
+++ src/routers/login/router.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import * as mongoose from 'mongoose';
 import { LoginController } from './controller';
-import { Login } from '../../models';
+import { Login, User } from '../../models';
 
 export const loginRouter = () => {
   const router = Router();
-  const logincontroller = LoginController(Login);
+  const logincontroller = LoginController(Login, User);
   router.route('/')
     .post(logincontroller.post);
 
