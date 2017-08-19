91d179064060770e979b476173d90c69a56895cf
nasdan <daniel.sanchez@lemoncode.net>
2017-08-19 15:43:04 +0200

    Modify response to deployed version

--- src/routers/login/controller.ts
+++ src/routers/login/controller.ts
@@ -13,18 +13,21 @@ export const LoginController = (Login: Model<LoginModel>, User: Model<UserModel>
   };
 
   const accessGrant = (res: Response, loginModel: LoginModel) => {
-    User.findOne({ email: loginModel.email })
-      .exec()
-      .then(handleUserRequest(res))
-      .catch(() => res.sendStatus(400));
-  };
-
-  const handleUserRequest = (res: Response) => (userModel: UserModel) => {
     const securityToken = randomString(18);
     generateCookie(res, env.ACCESS_TOKEN_HEADER, securityToken);
-    res.status(201)
-      .send(userModel);
+    res.sendStatus(201);
+    // User.findOne({ email: loginModel.email })
+    //   .exec()
+    //   .then(handleUserRequest(res))
+    //   .catch(() => res.sendStatus(400));
   };
+
+  // const handleUserRequest = (res: Response) => (userModel: UserModel) => {
+  //   const securityToken = randomString(18);
+  //   generateCookie(res, env.ACCESS_TOKEN_HEADER, securityToken);
+  //   res.status(201)
+  //     .send(securityToken);
+  // };
 
   const accessDenied = (res: Response) => {
     res.sendStatus(401);
