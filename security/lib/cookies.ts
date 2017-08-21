f02036a2c9e774e5059e4f0a588c16007a99e6d8
nasdan <daniel.sanchez@lemoncode.net>
2017-08-21 11:27:12 +0200

    Add secure flag

--- security/lib/cookies.ts
+++ security/lib/cookies.ts
@@ -4,6 +4,7 @@ export const generateCookie = (res: Response, name: string, value) => {
   if (res.cookie) {
     res.cookie(name, value, {
       httpOnly: true,
+      secure: true,
     });
   }
 };
